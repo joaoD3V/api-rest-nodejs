c733480eb097219f075e8182c4bfd71bd0d86988
Joao Silva <joao.devweb@gmail.com>
2023-06-01 19:42:59 -0300

    fix: Production config

--- src/database.ts
+++ src/database.ts
@@ -2,9 +2,9 @@ import { knex as setupKnex, Knex } from 'knex';
 import { env } from './env';
 
 export const config: Knex.Config = {
-  client: env.DATABSE_CLIENT,
+  client: env.DATABASE_CLIENT,
   connection:
-    env.DATABSE_CLIENT === 'sqlite'
+    env.DATABASE_CLIENT === 'sqlite'
       ? {
           filename: env.DATABASE_URL,
         }
